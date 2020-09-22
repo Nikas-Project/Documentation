0550d39227a27d139dcc6d99581678a3933918ff
Arash Hatami <hatamiarash7@gmail.com>
2020-09-22 11:15:40 +0330

    Delete formgir docs

--- sidebars.js
+++ sidebars.js
@@ -1,10 +1,6 @@
 module.exports = {
     someSidebar: {
-        "فرم گیر": ["00-description"],
-        "شروع به کار": [
-            "01-prerequisites",
-            "02-create-website",
-            "03-configure-form",
-        ],
+        نیکاس: ["00-description"],
+        "شروع به کار": [],
     },
 };
